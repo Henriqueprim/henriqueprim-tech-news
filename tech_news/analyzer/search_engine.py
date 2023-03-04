9fc5fb7ab7d922932f2b1e83e719db28cf0d4207
Henrique Salm Prim <henrique_salmprim@hotmail.com>
2023-03-04 20:09:36 -0300

    feat: implements search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = search_news({"title": re.compile(rf"{title}", re.I)})
+
+    return [(news['title'], news['url']) for news in news_list]
 
 
 # Requisito 8
