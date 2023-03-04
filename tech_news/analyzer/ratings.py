fd2257eb5926b6b8aa83ca8287cbee4f4e431944
Henrique Salm Prim <henrique_salmprim@hotmail.com>
2023-03-04 20:50:23 -0300

    feat: implements top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,12 @@
+from collections import Counter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    if news_list:
+        category_list = Counter(sorted(
+            news['category'] for news in news_list)).most_common(5)
+        return [category[0] for category in category_list]
+    return []
